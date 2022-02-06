18e1a7e4ca151043c9c6ce243941c546cdde7780
ksigei <sigeikiprono4@gmail.com>
2022-02-06 14:17:09 +0300

    created a blog

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def post_list(request):
+    posts = Post.published.all()
+    
+    paginator = Paginator(posts, 10) # 10 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+        
+    return render(request,'post_list.html',{'posts':posts, page:'pages'})
+
+def post_detail(request, post):
+    post=get_object_or_404(Post,slug=post,status='published')
+    return render(request, 'post_detail.html',{'post':post})
