4e433e8b7f9d3985a7d64f6b9c7087eaf6d31be7
ksigei <sigeikiprono4@gmail.com>
2022-02-10 07:34:30 +0300

    Deploying schools22 to heroku

--- schoolblog/settings.py
+++ schoolblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-m=39n5z(kqxnr=tnc6fb%q5grnhctghx#2jv3ze%!-y0!r&+#)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['schools22.herokuapp.com', 'localhost','*']
 
