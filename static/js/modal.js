935e297e80694b476dc25a02d746f0fd827f53c4
ksigei <sigeikiprono4@gmail.com>
2022-02-14 02:37:57 -0500

    Added modal.js

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,14 +1,12 @@
-{% extends 'post_list.html' %}
-
-{% block title %} - {{post.title}}{% endblock title %}
-
-{% block content %}
+function showMore() {
+var modal = document.getElementById('blog');
+modal.innerHTML += `
     <article>
         <figure>
          <img src="{{ post.image.url }}" class="rounded" alt="{{post.title}}">
         </figure>
         <div class="mt-3">
-            <h1>{{post.title}}</h1>
+            <h1>${post.title}</h1>
             <p class="text-muted">
                 Published {{ post.publish.date }} by <strong>{{ post.author }}</strong>
             </p>
@@ -20,5 +18,7 @@
         
     </article>
     
-   
-{% endblock content %}
+                                                               
+`
+}
+
